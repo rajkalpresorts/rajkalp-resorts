9bf2afcc5b314f6aedff26dd343396689eec124a
nkraj7266 <nkraj7266@gmail.com>
2024-04-26 12:47:31 +0530

    tree access

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,48 +1,59 @@
 import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import connectDB from "@/database/dbConfig";
 import User from "@/database/models/user";
 
 export async function POST(req) {
-    try {
-        await connectDB();
+	try {
+		await connectDB();
 
-        const reqBody = await req.json()
-        const { email, password } = reqBody;
+		const reqBody = await req.json();
+		const { email, password } = reqBody;
 
-        const user = await User.findOne({ email });
+		const user = await User.findOne({ email });
 
-        if (!user) {
-            return NextResponse.json({ error: "No user found" }, { status: 400 })
-        }
+		if (!user) {
+			return NextResponse.json(
+				{ error: "No user found" },
+				{ status: 400 }
+			);
+		}
 
-        const isMatch = await user.matchPassword(password);
+		const isMatch = await user.matchPassword(password);
 
-        if (!isMatch) {
-            return NextResponse.json({ error: "Invalid credentials" }, { status: 400 })
-        }
+		if (!isMatch) {
+			return NextResponse.json(
+				{ error: "Invalid credentials" },
+				{ status: 400 }
+			);
+		}
 
-        const tokenData = {
-            id: user._id,
-            email: user.email,
-            firstName: user.firstName,
-            lastName: user.lastName,
-        }
+		const tokenData = {
+			id: user._id,
+			email: user.email,
+			firstName: user.firstName,
+			lastName: user.lastName,
+			role: user.role,
+		};
 
-        const token = jwt.sign(tokenData, process.env.TOKEN_SECRET, { expiresIn: "1d" })
+		const token = jwt.sign(tokenData, process.env.TOKEN_SECRET, {
+			expiresIn: "1d",
+		});
 
-        const response = NextResponse.json({
-            message: "Login successful",
-            user: tokenData,
-        }, { status: 200 })
+		const response = NextResponse.json(
+			{
+				message: "Login successful",
+				user: tokenData,
+			},
+			{ status: 200 }
+		);
 
-        response.cookies.set("token", token, {
-            httpOnly: true,
-        })
+		response.cookies.set("token", token, {
+			httpOnly: true,
+		});
 
-        return response;
-
-    } catch (error) {
-        return NextResponse.json({ error: error.message }, { status: 500 })
-    }
+		return response;
+	} catch (error) {
+		return NextResponse.json({ error: error.message }, { status: 500 });
+	}
 }
--- src/app/api/tree/route.js
+++ src/app/api/tree/route.js
@@ -4,64 +4,79 @@ import jwt from "jsonwebtoken";
 import User from "@/database/models/user";
 
 const fetchReferrals = async (userId, depth) => {
-    const user = await User.findById(userId).lean();
+	const user = await User.findById(userId).lean();
 
-    if (!user) {
-        return NextResponse.json({
-            error: "User not found!"
-        }, { status: 404 });
-    }
+	if (!user) {
+		return NextResponse.json(
+			{
+				error: "User not found!",
+			},
+			{ status: 404 }
+		);
+	}
 
-    if (depth === 0 || !user.referrals.length) {
-        return {
-            name: user.firstName + ' ' + user.lastName,
-            attributes: {
-                email: user.email,
-                referralId: user.referralId,
-            },
-            children: [],
-        };
-    }
+	if (depth === 0 || !user.referrals.length) {
+		return {
+			name: user.firstName + " " + user.lastName,
+			attributes: {
+				email: user.email,
+				referralId: user.referralId,
+			},
+			children: [],
+		};
+	}
 
-    const referrals = await Promise.all(user.referrals.map(referralId => fetchReferrals(referralId, depth - 1)));
+	const referrals = await Promise.all(
+		user.referrals.map((referralId) =>
+			fetchReferrals(referralId, depth - 1)
+		)
+	);
 
-    return {
-        name: user.firstName + ' ' + user.lastName,
-        attributes: {
-            email: user.email,
-            referralId: user.referralId,
-        },
-        children: referrals,
-    };
-}
+	return {
+		name: user.firstName + " " + user.lastName,
+		attributes: {
+			email: user.email,
+			referralId: user.referralId,
+		},
+		children: referrals,
+	};
+};
 
 export async function POST(req) {
-    try {
-        await connectDB();
+	try {
+		await connectDB();
 
-        const token = req.cookies.get('token')?.value || null;
+		const token = req.cookies.get("token")?.value || null;
 
-        if (!token) {
-            return NextResponse.json({
-                error: "No access token!"
-            }, { status: 300 });
-        }
+		if (!token) {
+			return NextResponse.json(
+				{
+					error: "No access token!",
+				},
+				{ status: 300 }
+			);
+		}
 
-        const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+		const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
 
-        if (decoded.role !== 'admin' && decoded.role !== 'user') {
-            return NextResponse.json({
-                error: "Unauthorized access!"
-            }, { status: 401 });
-        }
+		if (decoded.role !== "admin" && decoded.role !== "user") {
+			return NextResponse.json(
+				{
+					error: "Unauthorized access!",
+				},
+				{ status: 401 }
+			);
+		}
 
-        const { userId } = await req.json();
-        const tree = await fetchReferrals(userId, 4);
-        return NextResponse.json(tree, { status: 200 });
-
-    } catch (error) {
-        return NextResponse.json({
-            error: error.message
-        }, { status: 500 });
-    }
+		const { userId } = await req.json();
+		const tree = await fetchReferrals(userId, 4);
+		return NextResponse.json(tree, { status: 200 });
+	} catch (error) {
+		return NextResponse.json(
+			{
+				error: error.message,
+			},
+			{ status: 500 }
+		);
+	}
 }
